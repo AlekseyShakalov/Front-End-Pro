d876fc53beccc69d76d4783de886e4d5e359ad23
AlekseyShakalov <wellendy.1@gmail.com>
2022-08-08 01:26:46 +0300

    added homework_6

---
+++ 6/js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+
+// task_1
+let name = prompt("Как тебя зовут?");
+alert(name);
+
+
+// task_2
+let a = 4;
+let b = 2;
+
+alert(a + b);
+alert(a - b);
+alert(a * b);
+alert(a / b);
+alert(a ** b);
