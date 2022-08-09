3d5f014c5b8e624352dbed17f1490146a22d970f
AlekseyShakalov <wellendy.1@gmail.com>
2022-08-09 23:28:01 +0300

    added homework_7

---
+++ 7/js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+
+// // task_1
+// let sumTo = n => n === 1 ? n : n + sumTo(n-1)
+
+// console.log(sumTo(4));
+// console.log(sumTo(3)); 
+
+
+// // task_2
+// let findShort = str => str.split(" ").sort((a, b) => a.length - b.length)[0];
+
+// const sentance = "Lorem ipsum dolor sit amet";
+
+// console.log(findShort(sentance)); 
+// console.log(findShort("Hello world")); 
+// console.log(findShort("Hi")); 
+// console.log(findShort("She is David's sister")); 
