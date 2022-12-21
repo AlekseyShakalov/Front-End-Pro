354d0d4dcbeb7314b3d13b9980434b2890406d9a
AlekseyShakalov <wellendy.1@gmail.com>
2022-12-21 02:37:52 +0200

    added homework_30

---
+++ 30/src/App.js
@@ -0,0 +1,13 @@
+import InvestmantCard from "./components/InvestmantCard";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <InvestmantCard />
+    </div>
+  );
+}
+
+export default App;
